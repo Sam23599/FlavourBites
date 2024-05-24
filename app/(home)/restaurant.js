91f9e19c11e7e7b9ebae34dceb28afc0c8a3f0e9
Satyam Singh Virat <satyam23599@gmail.com>
2024-05-24 17:48:40 +0530

    smooth scroll on menu catg in restraus added

--- app/(home)/restaurant.js
+++ app/(home)/restaurant.js
@@ -1,13 +1,14 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { router, useLocalSearchParams, useRouter } from 'expo-router';
+import { Pressable, ScrollView, StyleSheet, Text, View, Image, Animated } from 'react-native'
+import React, { useRef } from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import { Ionicons, SimpleLineIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-import index from '..';
 import FoodItem from '../../components/FoodItem';
+import { useSelector } from 'react-redux';
 
 const restaurant = () => {
   const params = useLocalSearchParams();
   const router = useRouter();
+  const cart = useSelector((state) => state.cart.cart);
 
   const menu = [
     {
@@ -147,36 +148,76 @@ const restaurant = () => {
     },
   ];
 
+  const scrollViewRef = useRef(null);
+  const scrollAnimation = useRef(new Animated.Value(0)).current;
+  const item_height = 650;
+  const scrollToCategory = (index) => {
+    const yOffSet = index * item_height;
+    Animated.timing(scrollAnimation, {
+      toValue: yOffSet,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+    scrollViewRef.current.scrollTo({ y: yOffSet, animated: true })
+  };
+
   return (
-    <ScrollView style={{ backgroundColor: "white" }}>
-      <View style={{ marginTop: 5, flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
-        <Ionicons onPress={() => router.back()} style={{ padding: 5 }} name="arrow-back" size={24} color="black" />
-        <View style={{ flexDirection: "row", alignItems: "center", paddingHorizontal: 24, gap: 10 }}>
-          <SimpleLineIcons name='camera' size={24} color="black" />
-          <Ionicons name='bookmark-outline' size={24} color="black" />
-          <MaterialCommunityIcons name='share-outline' size={24} color="black" />
-        </View>
-      </View>
-
-      <View style={{ justifyContent: "center", alignItems: "center", marginVertical: 12 }}>
-        <Text style={{ fontSize: 20, fontWeight: "bold" }}>{params?.name}</Text>
-        <Text style={{ marginTop: 5, color: "gray", fontWeight: "500", fontSize: 15 }}>North Indian - Fast Food - 160 for one</Text>
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 4, marginTop: 10 }}>
-          <View style={{ flexDirection: "row", alignItems: "center", backgroundColor: "#006A4E", borderRadius: 4, paddingHorizontal: 4, paddingVertical: 5, gap: 4 }}>
-            <Text style={{ color: "white", fontSize: 14, fontWeight: "bold" }}>{params?.aggregate_rating}</Text>
-            <Ionicons name='star' size={15} color="white" />
+    <>
+      <ScrollView ref={scrollViewRef} style={{ backgroundColor: "white" }}>
+        <View style={{ marginTop: 5, flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+          <Ionicons onPress={() => router.back()} style={{ padding: 5 }} name="arrow-back" size={24} color="black" />
+          <View style={{ flexDirection: "row", alignItems: "center", paddingHorizontal: 24, gap: 10 }}>
+            <SimpleLineIcons name='camera' size={24} color="black" />
+            <Ionicons name='bookmark-outline' size={24} color="black" />
+            <MaterialCommunityIcons name='share-outline' size={24} color="black" />
           </View>
-          <Text style={{ fontSize: 15, fontWeight: "500", marginLeft: 5 }}>3.2K Rating</Text>
         </View>
-        <View style={{ justifyContent: "center", alignItems: "center", backgroundColor: "#D0F0C0", borderRadius: 20, paddingHorizontal: 10, paddingVertical: 5, marginTop: 12 }}>
-          <Text>30-40 mins | 6km | Hyderbad</Text>
+
+        <View style={{ justifyContent: "center", alignItems: "center", marginVertical: 12 }}>
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>{params?.name}</Text>
+          <Text style={{ marginTop: 5, color: "gray", fontWeight: "500", fontSize: 15 }}>North Indian - Fast Food - 160 for one</Text>
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 4, marginTop: 10 }}>
+            <View style={{ flexDirection: "row", alignItems: "center", backgroundColor: "#006A4E", borderRadius: 4, paddingHorizontal: 4, paddingVertical: 5, gap: 4 }}>
+              <Text style={{ color: "white", fontSize: 14, fontWeight: "bold" }}>{params?.aggregate_rating}</Text>
+              <Ionicons name='star' size={15} color="white" />
+            </View>
+            <Text style={{ fontSize: 15, fontWeight: "500", marginLeft: 5 }}>3.2K Rating</Text>
+          </View>
+          <View style={{ justifyContent: "center", alignItems: "center", backgroundColor: "#D0F0C0", borderRadius: 20, paddingHorizontal: 10, paddingVertical: 5, marginTop: 12 }}>
+            <Text>30-40 mins | 6km | Hyderbad</Text>
+          </View>
         </View>
+
+        {menu?.map((item, index) => (
+          <FoodItem key={index} item={item} />
+        ))}
+      </ScrollView>
+
+      <View style={{ flexDirection: "row", backgroundColor: "white" }}>
+        {menu?.map((item, index) => (
+          <Pressable
+            onPress={() => scrollToCategory(index)}
+            style={{ paddingHorizontal: 7, borderRadius: 4, paddingVertical: 5, marginVertical: 10, marginHorizontal: 10, alignItems: "center", justifyContent: "center", borderColor: "#181818", borderWidth: 1 }}>
+            <Text>
+              {item?.name}
+            </Text>
+          </Pressable>
+        ))}
       </View>
 
-      {menu?.map((item, index) => (
-        <FoodItem key={index} item={item} />
-      ))}
-    </ScrollView>
+      {cart?.length > 0 && (
+        <Pressable style={{ backgroundColor: "#fd5c63", paddingHorizontal: 10, paddingVertical: 10, justifyContent: "center", alignItems: "center" }} >
+          <Text style={{ textAlign: "center", color: "white", marginTop: 5, fontWeight: "600" }}>
+            {cart.length} items added
+          </Text>
+          <Text style={{ textAlign: "center", color: "white", marginTop: 5, fontWeight: "600" }}>
+            Add items worth 400 to reduce surge fee by Rs 50.
+          </Text>
+        </Pressable>
+      )}
+
+
+    </>
   )
 }
 
